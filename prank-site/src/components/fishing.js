dfab174fb745050256a4be33c1ab27acfe309abb
松田　雄飛 <k20109kk@aitech.ac.jp>
2022-03-17 14:22:45 +0900

    add:countdown

--- prank-site/src/components/fishing.js
+++ prank-site/src/components/fishing.js
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import { Link } from "react-router-dom";
+import Countdown from 'react-countdown'
 export default function fishing() {
     return (
-        <div className="profile">
-            {/* ここのカウントダウンなどのアレを書く */}
+        <div>
+            <Countdown date={Date.now() + 100000} />
         </div>
     )
 }
